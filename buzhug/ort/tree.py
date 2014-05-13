9a03148bdc6d3a44debee7e1bbd414e01532eac8
stephanie <swang93@mit.edu>
2014-05-12 21:06:17 -0400

    this does not work...

--- buzhug/ort/tree.py
+++ buzhug/ort/tree.py
@@ -1,6 +1,6 @@
 import os
 from node import *
-from serializer.base import Serializer
+from serializer.line_serializer import LineSerializer
 
 # Build a tree from a list of generic objects, represented as lists of (key,
 # value) tuples. Each object should have the same d keys, each one of which
@@ -8,13 +8,12 @@ from serializer.base import Serializer
 # not, everything will fuck up. Don't make everything fuck up.
 # seq is the sequence of keys in the first item of the list of data.
 def build_tree(data, B, filename):
-    serializer = Serializer(filename)
-    # root will have an offset of 0
-    root = serializer.deserialize_node()
-    if root != None:
-        return
-
-    build_tree(data)
+    serializer = LineSerializer(filename)
+    # root will have a block position of 0
+    if serialier.read_mode:
+        # if we're already in read mode, then the file's already been built, so
+        # return the first node in file
+        return serializer.loads(0)
 
     # If the sequence of keys is not the same in every other data item
     # as well, you fucked up. We make it immutable just to be safe.
@@ -30,41 +29,49 @@ def build_tree(data, B, filename):
     data.sort(key=lambda dp: dp[0][1])
 
     # Now that we've got leaves, let's build their parents, recursively.
-    # The output of the function is a single node: the root.
-    root = build_upwards(data, RangeLeaf)
-    return root
+    # serialize NOW! :)
+    build_upwards(data, B, RangeLeaf, serializer)
+    serializer.flush()
+    # and output the root
+    return serializer.loads(0)
 
-# This is a function to build nodes recursively, bottom-up. It takes
-# chunks of B of whatever data type you pass into children, puts
-# them into whatever class you set as NodeClass, and recurses up.
-# I think bottom-up should minimize the total number of file blocks
-# the system uses compared to top-down, but could be wrong.
-def build_upwards(children, NodeClass):
+def build_upwards(children, B, NodeClass, serializer):
+    """
+    Function to build nodes recursively, bottom-up. Takes chunks of B of
+    whatever data type you pass into children, puts them into whatever class
+    you set as NodeClass, and recurses up
+    Tree gets built in reverse order of range query recursion, so after making
+    the level with all the parents the serialization order here is:
+        for each parent from left to right (okay to do range query from right to
+        left on single level):
+            - linked tree in next dimension
+            - parent
+        then recurse with the parents as the children of next level
+    """
     parents = []
-    while children:
-        # If there are more than one parent's worth of children, chop
+    
+    i = 0
+    while (i + 1) * B <= len(children):
+        # If there is more than one parent's worth of children, chop
         # off the first B in a chunk
-        if len(children) > B:
-            cluster = children[:B]
-            children = children[B:]
-        else:
-            cluster = children[:]
-            children = []
+        cluster = children[i * B:(i + 1) * B]
 
-        # actually make a parent
-        parent = NodeClass(cluster[:]) # with a copy, no references
-        parents.append(parent)  # and add it to our collection
+        # serialize parent's linked tree in next dimension
+        # TODO: fix this...
+        build_tree(data, B)
+        parent = NodeClass(cluster)
+        # then serialize parent
+        serializer.dumps([parent])
+
+        parents.append(parent) # and add it to our collection
+        i += 1
 
     # If we ended up with more than one parent, we need to give them
     # their own parents.
     # R E C U R S E
     if len(parents) > 1:
         # On the recursive steps, the parents are always RangeNodes.
-        return build_upwards(parents, RangeNode)
-
-    # Otherwise, we've got the root. Bam.
-    return parents[0]
-
+        return build_upwards(parents, B, RangeNode, serializer)
 
 if __name__ == '__main__':
     data = [[(i, random.random()) for i in ['x', 'y', 'z']] for j in
