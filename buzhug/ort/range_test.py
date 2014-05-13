a72aabde237b2207ce66da4cae6fb77134acac5d
stephanie <swang93@mit.edu>
2014-05-13 04:10:08 -0400

    unit test for tree

---
+++ buzhug/ort/range_test.py
@@ -0,0 +1,17 @@
+import os
+import tree 
+from serializer.line_serializer import LineSerializer
+
+def build_test():
+    data = []
+
+    for j in range(10):
+        data_item = [("field" + str(i), i * j) for i in range(3)]
+        data.append(data_item)
+
+    tree_file = 'test-tree'
+    if os.path.isfile(tree_file):
+        os.remove(tree_file)
+    serializer = LineSerializer(tree_file) 
+
+    root = tree.build_tree(data, 2, serializer)
