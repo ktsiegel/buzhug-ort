8424776183d15ad7c122ccf12b2790e0863bd3b3
stephanie <swang93@mit.edu>
2014-05-13 13:41:29 -0400

    unit tests rawr

--- buzhug/ort/range_test.py
+++ buzhug/ort/range_test.py
@@ -16,14 +16,34 @@ def build_test():
         os.remove(tree_file)
     serializer = LineSerializer(tree_file)
 
-    root = tree.build_tree(data, 3, serializer)
-    root2 = serializer.loads(root.linked_node) 
+    root1 = tree.build_tree(data, 3, serializer)
+    root2 = serializer.loads(root1.linked_node) 
     root3 = serializer.loads(root2.linked_node)
 
-    assert len(root.get_all_data()) == len(data) 
+    dim1 = [data_item[1] for data_item in
+            sorted((data_item[0] for data_item in data), key=lambda d: d[1])]
+    dim2 = [data_item[1] for data_item in
+            sorted((data_item[1] for data_item in data), key=lambda d: d[1])]
+    dim3 = [data_item[1] for data_item in
+            sorted((data_item[2] for data_item in data), key=lambda d: d[1])]
+
+    # make sure all the roots have all values at bottom
+    assert len(root1.get_all_data()) == len(data) 
     assert len(root2.get_all_data()) == len(data) 
     assert len(root3.get_all_data()) == len(data) 
 
-    print root.children
-    
-    assert False 
+    # check intervals on each level
+    for root, dim in zip([root1, root2, root3], [dim1, dim2, dim3]):
+        root_children = [root.load_child(child) for child in root.children]
+        root_grandchildren = [child.load_child(grandchild) for child in root_children for grandchild in child.children]
+
+        assert root_children[0].min == dim[0]
+        assert root_children[0].max == dim[8]
+        assert root_children[1].min == dim[9]
+        assert root_children[1].max == dim[9]
+
+        for i in range(3):
+            assert root_grandchildren[i].min == dim[i * 3]
+            assert root_grandchildren[i].max == dim[i * 3 + 2]
+
+    print serializer.back_seeks
